57aac3c0891b6cb93e593445bad9b96b862f3240
ErnieJu <ernie971009@gmail.com>
2021-08-18 18:56:37 +0900

    데이터 넣어보기

--- src/SearchRst.js
+++ src/SearchRst.js
@@ -3,12 +3,12 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import axios from 'axios';
 import ReactPaginate from 'react-paginate';
-import searchResults from './searchResult';
+import SearchData from './SearchData';
 
 
 function SearchRst(props){
 
-  let [searchResult, changesearchResult] = useState(searchResults);
+  let [searchResult, changesearchResult] = useState(SearchData);
   let {id} = useParams();
   let history = useHistory();
 
@@ -73,6 +73,8 @@ function SearchRst(props){
         <Col xs={1}>조회수</Col>
       </Row>
     </Container>
+    &nbsp;
+    {showQna}
     &nbsp;
     <ReactPaginate
       previousLabel={"<"}
