75c6f4030848249516472af49074f016b3a0a698
ErnieJu <ernie971009@gmail.com>
2021-08-23 22:03:06 +0900

    컨디션 최악..

--- src/Cart.js
+++ src/Cart.js
@@ -1,54 +1,72 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ListGroup, Button, Container, Row, Col } from 'react-bootstrap';
 import './App.css';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import {connect} from 'react-redux';
+import CartData from './CartData.js';
+import axios from 'axios';
+
+
 
 function Cart(props) {
-    let history = useHistory();
-    let product = useState(['']);
-    let {id} = useParams();
+  let history = useHistory();
+  let product = useState(['']);
+  let {id} = useParams();
+  let [cartData, changecartData] = useState(CartData);
 
-    return(
-        <Container fluid="md" className='cart_back'>
-            <Row>
-                <Col>
-                    <h1>
-                        My Cart    
-                    </h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={2} >
-                    left
-                </Col>
-                <Col xs={8} className='cart_middle'>
-                    
-                    {/* 
-                    props.state.map((a,i)=>{
-                        return (
-                            <tr key={i}>
-                                <td>{ a.id }</td>
-                                <td>{ a.name }</td>
-                                <td>{ a.id }</td>
-                            </tr>
-                        )
-                    })
-                */}
-                   
-                </Col>
-                <Col xs={2}>
-                    right
-                </Col>
-            </Row>
-            <Row>
-                <Col> 
-                    <Button onClick={ ()=>{ history.push('/') } }>결제하기</Button>
-                    &nbsp;
-                    <Button onClick={ ()=>{ history.goBack() } }>뒤로가기</Button></Col>
-            </Row>
-        </Container>
-    )
+  const axios = require("axios");
+  useEffect(()=>{axios.get('http://localhost:8080/api/product/findAll')
+                  .then(({ data })=>{ 
+                    changecartData(data) 
+                  })
+                  .catch()},[]);
+
+  let detailPrdt = CartData.find(function(result){
+    return(result.id == id -1)
+  });
+
+
+  return(
+    <Container fluid="md" className='cart_back'>
+      <Row>
+        <Col>
+          <h1>
+            My Cart    
+          </h1>
+        </Col>
+      </Row>
+
+      <Container className='qnaList'>
+        <Row className='qnacategory'>
+          <Col xs={2}></Col>
+          <Col xs={8}>제품 목록</Col>
+          <Col xs={2}></Col>
+        </Row>
+      </Container>
+
+        <Row>
+          <Col xs={2} >
+            left
+          </Col>
+          <Col xs={8} className='cart_middle'>
+             <Col>
+             {cartData[2].name}
+             </Col>
+                
+          </Col>
+          <Col xs={2}>
+            right
+          </Col>
+        </Row>
+      <Row>
+        <Col> 
+          <Button onClick={ ()=>{ history.push('/') } }>결제하기</Button>
+          &nbsp;
+          <Button onClick={ ()=>{ history.goBack() } }>뒤로가기</Button>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 
---
+++ src/CartData.js
@@ -0,0 +1,38 @@
+export default [
+    {
+    id : 0,
+    name : "ProductA",
+    description : "This is the ProductA",
+    cost : 39800,
+    stockQuantity : 3000,
+    imageUrl : "Null"
+    },
+
+    {
+    id : 1,
+    name : "ProductB",
+    description : "This is the ProductB",
+    cost : 26000,
+    stockQuantity : 3000,
+    imageUrl : "Null"
+    },
+
+    {
+    id : 2,
+    name : "ProductC",
+    description : "This is the ProductC",
+    cost : 9500,
+    stockQuantity : 3000,
+    imageUrl : "Null"
+    },
+
+    {
+    id : 3,
+    name : "ProductD",
+    description : "This is the ProductD",
+    cost : 63000,
+    stockQuantity : 3000,
+    imageUrl : "Null"
+    }
+
+]
